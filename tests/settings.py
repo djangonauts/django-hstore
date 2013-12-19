a6bc0355d242c26eff460ca148617b384c200481
nemesisdesign <nemesis@ninux.org>
2013-12-20 00:24:31 +0100

    added .travis.yml for travis-ci.org

--- tests/settings.py
+++ tests/settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         # possible backends are:
         #   * django.db.backends.postgresql_psycopg2
         #   * django.contrib.gis.db.backends.postgis
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_hstore',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
