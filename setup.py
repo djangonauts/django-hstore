9f656e84b7f191194a4ae01eb07d8bad71c54ab2
scott c <scoarescoare@gmail.com>
2014-01-03 15:33:01 -0500

    Using get_packages to find all items including static and template dirs

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test
+from setuptools import setup
+import re
+import os
+import sys
 
 from django_hstore import get_version
 
@@ -10,6 +14,30 @@ class TestCommand(test):
         runtests()
 
 
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
 setup(
     name='django-hstore',
     version=get_version(),
@@ -18,7 +46,8 @@ setup(
     author='Federico Capoano (nemesisdesign)',
     license='BSD',
     url='https://github.com/nemesisdesign/django-hstore',
-    packages=find_packages(exclude=['tests', 'tests.*', 'docs', 'docs.*']),
+    packages=get_packages('django_hstore'),
+    package_data=get_package_data('django_hstore'),
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
