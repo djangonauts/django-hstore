2ec0e7eb9c63af7f392d7b81757240696a875a56
Andrey Antukh <niwi@niwi.be>
2014-01-06 15:53:45 +0100

    Remove wrong usage of super call.

--- django_hstore/exceptions.py
+++ django_hstore/exceptions.py
@@ -5,4 +5,4 @@ class HStoreDictException(Exception):
 
     def __init__(self, *args, **kwargs):
         self.json_error_message = kwargs.pop('json_error_message', None)
-        super(self.__class__, self).__init__(*args, **kwargs)
+        super(HStoreDictException, self).__init__(*args, **kwargs)
