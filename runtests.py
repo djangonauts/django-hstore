1561ca977c80b468ae6997bcb9f682de1baca589
Andrey Antukh <niwi@niwi.be>
2014-01-26 16:13:43 +0100

    Move runtests.py to repo root.

--- runtests.py
+++ runtests.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 
 import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-sys.path.insert(0, "..")
+sys.path.insert(0, "tests")
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
