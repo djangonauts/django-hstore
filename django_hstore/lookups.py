1d050ccd6018cce2bd89bc716771c38ca2ed49f1
Fabio Corneti <info@corneti.com>
2014-03-27 21:06:57 +0100

    Added custom classes for icontains and contains lookups

--- django_hstore/lookups.py
+++ django_hstore/lookups.py
@@ -1,12 +1,18 @@
+from __future__ import unicode_literals, absolute_import
+
 from django.db.models.fields import Field
 from django.db.models.lookups import GreaterThan
 from django.db.models.lookups import GreaterThanOrEqual
 from django.db.models.lookups import LessThan
 from django.db.models.lookups import LessThanOrEqual
+from django.db.models.lookups import Contains
+from django.db.models.lookups import IContains
+from django.utils import six
 
 
 class HStoreComparisonLookupMixin(object):
     """
+    Mixin for hstore comparison custom lookups.
     """
 
     def as_comparison_sql(self, qn , connection):
@@ -56,8 +62,57 @@ class HStoreLessThanOrEqual(HStoreComparisonLookupMixin, LessThanOrEqual):
         return super(HStoreLessThanOrEqual, self).as_sql(qn, connection)
 
 
+class HStoreContains(Contains):
+
+    def as_postgresql(self, qn, connection):
+
+        l_db_type = self.lhs.output_type.db_type(connection=connection)
+        if l_db_type == 'hstore':
+            lhs, lhs_params = self.process_lhs(qn, connection)
+
+            #FIXME: the ::text cast is performed by psycopg2 on contains and icontains lookups, try to see if it can
+            # be overridden
+            lhs = lhs.replace('::text', '::hstore')
+
+            param = self.rhs
+
+            if isinstance(param, dict):
+                values = list(param.values())
+                keys = list(param.keys())
+
+                if len(values) == 1 and isinstance(values[0], (list, tuple)):
+                    return '%s->\'%s\' = ANY(%%s)' % (lhs, keys[0]), [[str(x) for x in values[0]]]
+
+                return '%s @> %%s' % lhs, [param]
+
+            elif isinstance(param, (list, tuple)):
+                if len(param) < 2:
+                    return '%s ? %%s' % lhs, [param[0]]
+
+                if param:
+                    return '%s ?& %%s' % lhs, [param]
+
+                raise ValueError('invalid value')
+
+            elif isinstance(param, six.string_types):
+                # if looking for a string perform the normal text lookup
+                # that is: look for occurence of string in all the keys
+                pass
+            else:
+                raise ValueError('invalid value')
+        return super(HStoreContains, self).as_sql(qn, connection)
+
+
+class HStoreIContains(IContains, HStoreContains):
+
+    pass
+
+
+
 #FIXME: what happens if another app has created a custom lookup?
 Field.register_lookup(HStoreGreaterThan)
 Field.register_lookup(HStoreGreaterThanOrEqual)
 Field.register_lookup(HStoreLessThan)
 Field.register_lookup(HStoreLessThanOrEqual)
+Field.register_lookup(HStoreContains)
+Field.register_lookup(HStoreIContains)
