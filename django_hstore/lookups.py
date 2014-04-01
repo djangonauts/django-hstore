d3455fdcfaa7627ac5d98f94df3e5d73aae35804
Fabio Corneti <info@corneti.com>
2014-04-01 17:59:28 +0200

    Removed useless db_type check in HStoreContains

--- django_hstore/lookups.py
+++ django_hstore/lookups.py
@@ -45,43 +45,40 @@ class HStoreLessThanOrEqual(HStoreComparisonLookupMixin, LessThanOrEqual):
 class HStoreContains(Contains):
 
     def as_postgresql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
 
-        l_db_type = self.lhs.output_type.db_type(connection=connection)
-        if l_db_type == 'hstore':
-            lhs, lhs_params = self.process_lhs(qn, connection)
+        #FIXME: ::text cast is added by ``django.db.backends.postgresql_psycopg2.DatabaseOperations.lookup_cast``;
+        # maybe there's a cleaner way to fix the cast for hstore columns
+        if lhs.endswith('::text'):
+            lhs = lhs[:-4] + 'hstore'
 
-            #FIXME: ::text cast is added by ``django.db.backends.postgresql_psycopg2.DatabaseOperations.lookup_cast``;
-            # maybe there's a cleaner way to fix the cast for hstore columns
-            if lhs.endswith('::text'):
-                lhs = lhs[:-4] + 'hstore'
+        param = self.rhs
 
-            param = self.rhs
+        if isinstance(param, dict):
+            values = list(param.values())
+            keys = list(param.keys())
 
-            if isinstance(param, dict):
-                values = list(param.values())
-                keys = list(param.keys())
+            if len(values) == 1 and isinstance(values[0], (list, tuple)):
+                return '%s->\'%s\' = ANY(%%s)' % (lhs, keys[0]), [[str(x) for x in values[0]]]
 
-                if len(values) == 1 and isinstance(values[0], (list, tuple)):
-                    return '%s->\'%s\' = ANY(%%s)' % (lhs, keys[0]), [[str(x) for x in values[0]]]
+            return '%s @> %%s' % lhs, [param]
 
-                return '%s @> %%s' % lhs, [param]
-
-            elif isinstance(param, (list, tuple)):
-                if len(param) == 0:
-                    raise ValueError('invalid value')
-
-                if len(param) < 2:
-                    return '%s ? %%s' % lhs, [param[0]]
-
-                if param:
-                    return '%s ?& %%s' % lhs, [param]
-
-            elif isinstance(param, six.string_types):
-                # if looking for a string perform the normal text lookup
-                # that is: look for occurence of string in all the keys
-                pass
-            else:
+        elif isinstance(param, (list, tuple)):
+            if len(param) == 0:
                 raise ValueError('invalid value')
+
+            if len(param) < 2:
+                return '%s ? %%s' % lhs, [param[0]]
+
+            if param:
+                return '%s ?& %%s' % lhs, [param]
+
+        elif isinstance(param, six.string_types):
+            # if looking for a string perform the normal text lookup
+            # that is: look for occurence of string in all the keys
+            pass
+        else:
+            raise ValueError('invalid value')
         return super(HStoreContains, self).as_sql(qn, connection)
 
 
