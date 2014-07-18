8de3a1d690bd047882062cd3f4dfe31a21d8f57f
Federico Capoano <nemesis@ninux.org>
2014-07-18 17:41:27 +0200

    schema: admin.py ensure virtual fields are removed

--- django_hstore/admin.py
+++ django_hstore/admin.py
@@ -4,10 +4,17 @@ from django import forms
 
 class HStoreSchemaForm(forms.ModelForm):
     def full_clean(self):
+        """
+        perform validation also on hstore virtual fields
+        """
         self.instance._add_hstore_virtual_fields_to_fields()
-        super(HStoreSchemaForm, self).full_clean()
-        self.instance._remove_hstore_virtual_fields_from_fields()
-
+        try:
+            super(HStoreSchemaForm, self).full_clean()
+        except Exception as e:
+            raise e
+        # ensure virtual fields are removed
+        finally:
+            self.instance._remove_hstore_virtual_fields_from_fields()
 
 class HStoreSchemaAdmin(admin.ModelAdmin):
     form = HStoreSchemaForm
