2c521a4aae46bff76b327b3a8bd68e6ab158a246
Federico Capoano <nemesis@ninux.org>
2014-08-25 12:46:20 +0200

    added python 3.4 to setup.py classifiers

--- setup.py
+++ setup.py
@@ -61,5 +61,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
