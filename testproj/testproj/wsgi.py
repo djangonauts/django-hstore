7fd7bad1c162315509d87245f62b3cbcb02020bb
Andrey Antukh <niwi@niwi.be>
2014-08-27 16:17:23 +0200

    Add testproject.

---
+++ testproj/testproj/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for testproj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+sys.path.insert(0, "..")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproj.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
