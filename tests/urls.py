0c7b8df6b89b0661aeb85192108802584c225ae1
Jarek Glowacki <jarekwg@gmail.com>
2016-07-06 20:23:51 +1000

    Update urlpatterns

--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 
-urlpatterns = patterns('',  # noqa
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 
 if 'grappelli' in settings.INSTALLED_APPS:
-    urlpatterns = urlpatterns + patterns('',  # noqa
+    urlpatterns.append([
         url(r'^grappelli/', include('grappelli.urls')),
-    )
+    ])
